3f6da1378691c6d697befd835b7eb4f321fda530
Mithali Sanjay Phadtare <phadtaremithali@gmail.com>
2025-11-21 14:31:57 +0530

    Implement triangle perimeter calculation

---
+++ triangle_perimeter.py
@@ -0,0 +1,8 @@
+a=int(input("enter the side of triangle:"))
+b=int(input("enter the another side of triangle:"))
+c=int(input("enter the another side of triangle:"))
+def perimeter(a,b,c):
+    perimeter=a+b+c
+    return perimeter
+
+print(perimeter(a,b,c))
