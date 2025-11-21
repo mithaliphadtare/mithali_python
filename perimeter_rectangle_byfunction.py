d62a3d1bbdadb0ef9b59424f4f26c51a85bf2e7a
Mithali Sanjay Phadtare <phadtaremithali@gmail.com>
2025-11-21 14:27:32 +0530

    Add function to calculate rectangle perimeter

---
+++ perimeter_rectangle_byfunction.py
@@ -0,0 +1,7 @@
+n=int(input("enter the width of rectangle:"))
+m=int(input("enter the height of rectangle:"))
+def per(n,m):
+    perimeter=2*(n+m)
+    return perimeter
+
+print("perimeter is:",per(n,m))
